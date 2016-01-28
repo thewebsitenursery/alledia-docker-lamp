7effd41438e7999bfda0fd337c1ec213428169ab
Anderson Grudtner Martins <anderson@ostraining.com>
2016-01-28 12:29:35 -0200

    Enable insecure key by default, for local development environments

--- run.sh
+++ run.sh
@@ -7,5 +7,8 @@ chown www-data:www-data -R /var/www
 service apache2 start
 service mysql start
 
+# Enable SSH - Insecure Key! - For development only
+/sbin/my_init --enable-insecure-key
+
 # Show the access log
 tail -f /var/log/apache2/access.log
